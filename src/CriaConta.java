fd8462182ae4413414274d3c4d83eabad0e522d8
PolegolasXD <polegolasplays@gmail.com>
2022-06-03 19:24:11 -0300

    feat: Finalizando conteudo basico.

--- src/CriaConta.java
+++ src/CriaConta.java
@@ -9,8 +9,24 @@ public class CriaConta {
 
 
         Conta segundaConta = new Conta();
-        segundaConta.saldo = 50;
+        segundaConta.saldo = 300;
         System.out.println("Primeira conta tem " + primeiraConta.saldo);
         System.out.println("segunda conta tem " + segundaConta.saldo);
+
+        primeiraConta.agencia = 01;
+        System.out.println(primeiraConta.agencia);
+        System.out.println(primeiraConta.numero);
+
+        segundaConta.agencia = 02;
+        System.out.println(segundaConta.agencia);
+        System.out.println(segundaConta.numero);
+
+        if(primeiraConta == segundaConta){
+            System.out.println("Mesma coisa");
+        }else{
+            System.out.println("São diferentes\n");
+        }
+        System.out.println(primeiraConta);
+        System.out.println(segundaConta);
     }
 }
