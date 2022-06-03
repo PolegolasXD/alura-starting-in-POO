3a826e275109a513542111a733cd8c2e081db165
PolegolasXD <polegolasplays@gmail.com>
2022-06-03 20:58:48 -0300

    feat: Conhecendo variações dentro de métodos.

--- src/Conta.java
+++ src/Conta.java
@@ -7,4 +7,23 @@ public class Conta{
   public void deposita(double valor){
     this.saldo += valor;
   }
+  public boolean saca (double valor){
+    if( this.saldo >= valor){
+      this.saldo -= valor;
+      return true;
+    }else {
+      return false;
+    }
+  }
+  public boolean transfere(double valor, Conta destino){
+    if (this.saldo >= valor){
+
+      this.saca(valor);
+      destino.deposita(valor);
+      return true;
+
+    } else {
+      return false;
+    }
+  }
 }
